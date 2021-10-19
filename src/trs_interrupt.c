27d2b3b02d28c3e73714ce9941c2b5e09c2f9d00
Jens Guenther <dbotw@gmx.net>
2021-10-19 07:25:38 +0200

    Tweak TCS Genie IIIs Disk INTRQ

--- src/trs_interrupt.c
+++ src/trs_interrupt.c
@@ -180,7 +180,7 @@ trs_timer_interrupt(int state)
 void
 trs_disk_intrq_interrupt(int state)
 {
-  if (trs_model == 1) {
+  if (trs_model == 1 && genie3s == 0) {
     if (state) {
       interrupt_latch |= M1_DISK_BIT;
       z80_state.irq = 1;
