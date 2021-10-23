0eb95f3433f24919d3685977e66c5aaa1d6ab93d
Jens Guenther <dbotw@gmx.net>
2021-10-23 15:43:07 +0200

    Redraw TCS Genie IIIs charset

--- src/trs_memory.c
+++ src/trs_memory.c
@@ -258,6 +258,9 @@ void genie3s_bank_out(int value)
 	if (value == genie3s)
 		return;
 
+	if ((value & (1 << 1)) != (genie3s & (1 << 1)))
+		trs_screen_refresh();
+
 	bank_base = (value & 0xC0) << 10; /* Bits 6 and 7: 64K Banks */
 	genie3s = value;
 }
