6176e74fbf5cc20f8638d14511b58b016318a446
Jens Guenther <dbotw@gmx.net>
2021-11-01 09:24:45 +0100

    Detect changed M6845 resolution

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -3389,31 +3389,29 @@ void m6845_cursor(int position, int line, int visible)
 
 void m6845_screen(int chars, int lines, int raster)
 {
-  if (chars)
+  int changed = 0;
+
+  if (chars && (changed = (chars != row_chars)))
     row_chars = chars;
 
-  if (lines)
+  if (lines && (changed = (lines != col_chars)))
     col_chars = lines;
 
-  if (genie3s) {
-    mem_video_page(row_chars != 64 && col_chars != 16);
-    if (m6845_raster != raster) {
-      m6845_raster = raster;
-      trs_screen_init();
-      return;
-    }
-  }
+  if (raster && (changed = (raster != m6845_raster)))
+    m6845_raster = raster;
 
-  if (screen_chars == row_chars * col_chars)
-    return;
+  if (changed) {
+    if (genie3s)
+      mem_video_page(row_chars != 64 && col_chars != 16);
 
-  screen_chars = row_chars * col_chars;
-  screen_init();
+    screen_chars = row_chars * col_chars;
+    screen_init();
 #if defined(SDL2) && !defined(_WIN32)
-  /* Needed for OpenBox */
-  SDL_HideWindow(window);
+    /* Needed for OpenBox */
+    SDL_HideWindow(window);
 #endif
-  trs_screen_init();
+    trs_screen_init();
+  }
 }
 
 void genie3s_char(int index, int address, int byte)
