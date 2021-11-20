cc55558faaa1dee8fcce16a636e9809ba79b3997
Jens Guenther <dbotw@gmx.net>
2021-11-20 18:32:51 +0100

    Add TCS Genie IIIs ESC key

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -2105,8 +2105,8 @@ void trs_get_event(int wait)
           keysym.sym = SDLK_LSHIFT;
 
         if (eg3200 || genie3s) {
-          /* 1-5 on numeric keypad for TCS Genie IIIs P1-P5 */
           if (genie3s) {
+            /* 1-5 on numeric keypad for TCS Genie IIIs P1-P5 */
 #ifdef SDL2
             if (keysym.sym >= SDLK_KP_1 && keysym.sym <= SDLK_KP_5)
               keysym.sym = (keysym.sym - SDLK_KP_1) + 0x88;
@@ -2114,6 +2114,9 @@ void trs_get_event(int wait)
             if (keysym.sym >= SDLK_KP1 && keysym.sym <= SDLK_KP5)
               keysym.sym = (keysym.sym - SDLK_KP1) + 0x88;
 #endif
+            /* End for Escape */
+            else if (keysym.sym == SDLK_END)
+              keysym.sym = 0x131;
           }
           if (keysym.sym == SDLK_LCTRL)
             keysym.sym = 0x12f;
