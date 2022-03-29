d371174e3913d3bcb17b0ec6bd03318fe49e566d
Jens Guenther <dbotw@gmx.net>
2022-03-29 15:04:03 +0200

    Add key binding for exit

--- src/trs_sdl_gui.c
+++ src/trs_sdl_gui.c
@@ -2331,6 +2331,7 @@ int trs_gui_joystick_get_button(void)
           case SDLK_F4:
 #endif
           case SDLK_q:
+          case SDLK_END:
             if (event.key.keysym.mod & KMOD_ALT)
               trs_exit(1);
             break;
