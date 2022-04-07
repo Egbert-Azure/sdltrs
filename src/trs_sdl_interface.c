9ded4b08928b460009c6e3ff0aa6c6eb47e1ea11
Jens Guenther <dbotw@gmx.net>
2022-04-07 16:30:31 +0200

    Set TCS Genie IIIs char width

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1251,7 +1251,7 @@ void trs_screen_init(void)
   }
 
   if (trs_model == 1) {
-    if (trs_charset < 3)
+    if (trs_charset < 3 && genie3s == 0)
       cur_char_width = 6;
     else
       cur_char_width = 8;
