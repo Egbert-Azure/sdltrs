524d1128ac7867da06b9329cd2411d7ab93754fe
Jens Guenther <dbotw@gmx.net>
2022-06-08 16:39:10 +0200

    Respect ESCape key binding for Genie III
    ... while here change keycode for TCS Genie IIIs ...

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -2162,7 +2162,7 @@ void trs_get_event(int wait)
 #endif
           /* End for Escape */
           if (keysym.sym == SDLK_END) {
-            keysym.sym = 0x131;
+            keysym.sym = eg3200 ? 0x111 : 0x8d;
             goto done;
           }
           if (keysym.sym == SDLK_LCTRL) {
