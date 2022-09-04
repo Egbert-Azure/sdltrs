2be3f8bc7c2235ccbbc3435129dfb2c77c1fc4b0
Jens Guenther <dbotw@gmx.net>
2022-09-04 10:26:03 +0200

    Remove extra CP-300/500 ROM vector

--- src/trs_clones.h
+++ src/trs_clones.h
@@ -51,7 +51,6 @@ struct model_quirks {
 
 extern struct model_quirks model_quirks;
 
-#define CP500_ROM_SIZE  (0x4000)  /* 16 KB */
 extern void cp500_reset_mode();
 extern Uint8 cp500_switch_mode(int mode);
 extern Uint8 cp500_mem_read(int address, int mem_map, Uint8 *rom, Uint8 *ram);
