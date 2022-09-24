cda49b1c62d9f9291654cbe34494d65ee2c12fb6
Jens Guenther <dbotw@gmx.net>
2022-09-24 17:16:34 +0200

    Sort prototypes in state header file

--- src/trs_state_save.h
+++ src/trs_state_save.h
@@ -23,47 +23,47 @@
 */
 
 int  trs_state_save(const char *filename);
+void trs_save_filename(FILE *file, char *filename);
+void trs_save_float(FILE *file, const float *buffer, int count);
+void trs_save_int(FILE *file, const int *buffer, int count);
+void trs_save_short(FILE *file, const short *buffer, int count);
 void trs_save_uint8(FILE *file, const Uint8 *buffer, int count);
 void trs_save_uint16(FILE *file, const Uint16 *buffer, int count);
 void trs_save_uint32(FILE *file, const Uint32 *buffer, int count);
 void trs_save_uint64(FILE *file, const Uint64 *buffer, int count);
-void trs_save_short(FILE *file, const short *buffer, int count);
-void trs_save_int(FILE *file, const int *buffer, int count);
-void trs_save_float(FILE *file, const float *buffer, int count);
-void trs_save_filename(FILE *file, char *filename);
 
 int  trs_state_load(const char *filename);
+void trs_load_filename(FILE *file, char *filename);
+void trs_load_float(FILE *file, float *buffer, int count);
+void trs_load_int(FILE *file, int *buffer, int count);
+void trs_load_short(FILE *file, short *buffer, int count);
 void trs_load_uint8(FILE *file, Uint8 *buffer, int count);
 void trs_load_uint16(FILE *file, Uint16 *buffer, int count);
 void trs_load_uint32(FILE *file, Uint32 *buffer, int count);
 void trs_load_uint64(FILE *file, Uint64 *buffer, int count);
-void trs_load_short(FILE *file, short *buffer, int count);
-void trs_load_int(FILE *file, int *buffer, int count);
-void trs_load_float(FILE *file, float *buffer, int count);
-void trs_load_filename(FILE *file, char *filename);
 
-void trs_main_save(FILE *file);
 void trs_cassette_save(FILE *file);
 void trs_disk_save(FILE *file);
 void trs_hard_save(FILE *file);
-void trs_stringy_save(FILE *file);
+void trs_imp_exp_save(FILE *file);
 void trs_interrupt_save(FILE *file);
 void trs_io_save(FILE *file);
-void trs_mem_save(FILE *file);
 void trs_keyboard_save(FILE *file);
+void trs_main_save(FILE *file);
+void trs_mem_save(FILE *file);
+void trs_stringy_save(FILE *file);
 void trs_uart_save(FILE *file);
 void trs_z80_save(FILE *file);
-void trs_imp_exp_save(FILE *file);
 
-void trs_main_load(FILE *file);
 void trs_cassette_load(FILE *file);
 void trs_disk_load(FILE *file);
 void trs_hard_load(FILE *file);
-void trs_stringy_load(FILE *file);
+void trs_imp_exp_load(FILE *file);
 void trs_interrupt_load(FILE *file);
 void trs_io_load(FILE *file);
-void trs_mem_load(FILE *file);
 void trs_keyboard_load(FILE *file);
+void trs_main_load(FILE *file);
+void trs_mem_load(FILE *file);
+void trs_stringy_load(FILE *file);
 void trs_uart_load(FILE *file);
 void trs_z80_load(FILE *file);
-void trs_imp_exp_load(FILE *file);
