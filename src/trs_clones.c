51277eb949fd410c7d4b1ac0662f82e6da61e37a
Jens Guenther <dbotw@gmx.net>
2022-09-25 14:25:51 +0200

    Simplify model quirks of TRS-80 clones

--- src/trs_clones.c
+++ src/trs_clones.c
@@ -1,75 +1,55 @@
 #include <stdio.h>
-
 #include "trs_clones.h"
 #include "trs_state_save.h"
 
+#define CLONES 7
+
 struct model_quirks model_quirks;
-static const struct model_quirks no_model_quirks;
-
-static const struct model_quirks cp500_quirks = {
-    "CP-500",
+static const struct model_quirks quirks[CLONES] = {
+  { NULL,
     0,
     0,
-};
+  },
 
-static const struct model_quirks cp500_m80_quirks = {
-    "CP-500 M80", /* name */
+  { "CP-500",
+    0,
+    0,
+  },
+
+  { "CP-500 M80", /* name */
     1, /* do not fire early disk interrupts */
     1, /* clear timer when latch is read */
-};
+  },
 
-static const struct model_quirks eg3200_quirks = {
-    "EACA EG 3200 Genie III",
+  { "EACA EG 3200 Genie III",
     0,
     0,
-};
+  },
 
-static const struct model_quirks genie3s_quirks = {
-    "TCS Genie IIIs",
+  { "TCS Genie IIIs",
     0,
     0,
-};
+  },
 
-static const struct model_quirks lnw80_quirks = {
-    "LNW80",
+  { "LNW80",
     0,
     0,
-};
+  },
 
-static const struct model_quirks speedmaster_quirks = {
-    "TCS SpeedMaster",
+  { "TCS SpeedMaster",
     0,
     0,
+  },
 };
 
 static int selected_clone;
 
 void trs_clone_quirks(int clone)
 {
-  switch (clone) {
-    case 0:
-      model_quirks = no_model_quirks;
-      break;
-    case CP500:
-      model_quirks = cp500_quirks;
-      break;
-    case CP500_M80:
-      model_quirks = cp500_m80_quirks;
-      break;
-    case EG3200:
-      model_quirks = eg3200_quirks;
-      break;
-    case GENIE3S:
-      model_quirks = genie3s_quirks;
-      break;
-    case LNW80:
-      model_quirks = lnw80_quirks;
-      break;
-    case SPEEDMASTER:
-      model_quirks = speedmaster_quirks;
-      break;
+  if (clone >= 0 && clone < CLONES) {
+    model_quirks = quirks[clone];
+    selected_clone = clone;
   }
-  selected_clone = clone;
 }
 
 void trs_clone_save(FILE *file)
