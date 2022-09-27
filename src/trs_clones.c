3492201e577ab7511ee5161999dc124a9f2ed82b
Jens Guenther <dbotw@gmx.net>
2022-09-27 20:08:32 +0200

    Reformat table of model quirks

--- src/trs_clones.c
+++ src/trs_clones.c
@@ -6,47 +6,18 @@
 
 struct model_quirks model_quirks;
 static const struct model_quirks quirks[CLONES] = {
-  { NULL, /* name of clone */
-    0,    /* use bit 4 for disk side select */
-    0,    /* do not fire early disk interrupts */
-    0,    /* clear timer when latch is read */
-  },
-
-  { "CP-500",
-    0,
-    0,
-    0,
-  },
-
-  { "CP-500 M80",
-    0,
-    1,
-    1,
-  },
-
-  { "EACA EG 3200 Genie III",
-    1,
-    0,
-    0,
-  },
-
-  { "TCS Genie IIIs",
-    1,
-    0,
-    0,
-  },
-
-  { "LNW80",
-    0,
-    0,
-    0,
-  },
-
-  { "TCS SpeedMaster",
-    1,
-    0,
-    0,
-  },
+  /*
+   *                          +--------- use bit 4 for disk side select
+   * name of clone            |  +------ do not fire early disk interrupts
+   *                          |  |  +--- clear timer when latch is read
+   *                          |  |  | */
+  { NULL,                     0, 0, 0, },
+  { "CP-500",                 0, 0, 0, },
+  { "CP-500 M80",             0, 1, 1, },
+  { "EACA EG 3200 Genie III", 1, 0, 0, },
+  { "TCS Genie IIIs",         1, 0, 0, },
+  { "LNW80",                  0, 0, 0, },
+  { "TCS SpeedMaster",        1, 0, 0, },
 };
 
 static int selected_clone;
