24d8729d060f0a160166d25a56f6cc4c7b3d0898
Jens Guenther <dbotw@gmx.net>
2022-10-06 17:36:32 +0200

    Add CP-500 emulation to state file

--- src/trs_state_save.c
+++ src/trs_state_save.c
@@ -47,6 +47,7 @@ int trs_state_save(const char *filename)
     trs_main_save(file);
     trs_cassette_save(file);
     trs_clone_save(file);
+    trs_cp500_save(file);
     trs_disk_save(file);
     trs_hard_save(file);
     trs_stringy_save(file);
@@ -87,6 +88,7 @@ int trs_state_load(const char *filename)
     trs_main_load(file);
     trs_cassette_load(file);
     trs_clone_load(file);
+    trs_cp500_load(file);
     trs_disk_load(file);
     trs_hard_load(file);
     trs_stringy_load(file);
