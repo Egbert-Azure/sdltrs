44a4c1e886bda4230f2dd5425ea7a312986229cc
Jens Guenther <dbotw@gmx.net>
2022-10-12 19:27:44 +0200

    Select TRS-80 clones with bit mask

--- src/trs_clones.c
+++ src/trs_clones.c
@@ -2,10 +2,8 @@
 #include "trs_clones.h"
 #include "trs_state_save.h"
 
-#define CLONES 7
-
 struct clone_quirks clone_quirks;
-static const struct clone_quirks quirks[CLONES] = {
+static const struct clone_quirks quirks[] = {
   /*
    *                                       +--------- use bit 4 for disk side select
    * Clone       name of clone             |  +------ do not fire early disk interrupts
@@ -27,9 +25,29 @@ void trs_clone_quirks(int clone)
   if (clone == current_clone)
     return;
 
-  if (clone >= 0 && clone < CLONES) {
-    clone_quirks = quirks[clone];
-    current_clone = clone;
+  current_clone = clone;
+
+  switch (clone) {
+    case CP500:
+      clone_quirks = quirks[1];
+      break;
+    case CP500_M80:
+      clone_quirks = quirks[2];
+      break;
+    case EG3200:
+      clone_quirks = quirks[3];
+      break;
+    case GENIE3S:
+      clone_quirks = quirks[4];
+      break;
+    case LNW80:
+      clone_quirks = quirks[5];
+      break;
+    case SPEEDMASTER:
+      clone_quirks = quirks[6];
+      break;
+    default:
+      clone_quirks = quirks[0];
   }
 }
 
