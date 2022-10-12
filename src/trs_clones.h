417e9c35fcc2047f17848177c59ed30120c53450
Jens Guenther <dbotw@gmx.net>
2022-10-12 19:27:44 +0200

    Select TRS-80 clones with bit mask

--- src/trs_clones.h
+++ src/trs_clones.h
@@ -2,12 +2,12 @@
 #define _TRS_CLONES_H
 
 /* Clones */
-#define CP500       (1)
-#define CP500_M80   (2)
-#define EG3200      (3)
-#define GENIE3S     (4)
-#define LNW80       (5)
-#define SPEEDMASTER (6)
+#define CP500       (1 << 1)
+#define CP500_M80   (1 << 2)
+#define EG3200      (1 << 3)
+#define GENIE3S     (1 << 4)
+#define LNW80       (1 << 5)
+#define SPEEDMASTER (1 << 6)
 
 /*
  * Some clones contain seem to behave in a slightly different way
